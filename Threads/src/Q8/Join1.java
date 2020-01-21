3e97d10ed1ccb10843930a426e305382ea923766
A00268807 <A00268807@X206-A07.studentnet.ait.ie>
2020-01-21 14:43:49 +0000

    Join Thread

---
+++ Threads/src/Q8/Join1.java
@@ -0,0 +1,34 @@
+package Q8;
+
+import java.awt.Graphics;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Join1 {
+	public static void main(String[] args) {
+		Thread t = new Thread() {
+			public void run() {
+				System.out.println("EnterValue:");
+				try {
+					System.in.read();
+				} catch (Exception ex) {
+				}
+				System.out.println("Thread Finished.");
+			}
+		};
+		System.out.println("Starting Thread");
+		t.start();
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+		}
+		try {
+			//t.join();
+		} catch (Exception e1) {
+		}
+		System.out.println("Join");
+		System.out.println("Main Finished.");
+	}
+}
---
+++ Threads/src/Q8/Join2.java
@@ -0,0 +1,30 @@
+package Q8;
+
+import java.awt.Graphics;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Join2 {
+	public static void main(String[] args) {
+		Thread t = new Thread() {
+			public void run() {
+				System.out.println("EnterValue:");
+				try {
+					System.in.read();
+				} catch (Exception ex) {
+				}
+				System.out.println("Thread Finished.");
+			}
+		};
+		System.out.println("Starting Thread");
+		t.start();
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+		}
+		System.out.println("No Join");
+		System.out.println("Main Finished.");
+	}
+}
