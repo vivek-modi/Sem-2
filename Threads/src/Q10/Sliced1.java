5cd957896f30ee8e597574a3b6e1ca09f3525498
A00268807 <A00268807@X206-A07.studentnet.ait.ie>
2020-01-21 15:15:20 +0000

    Sliced java

---
+++ Threads/src/Q10/Sliced1.java
@@ -0,0 +1,92 @@
+package Q10;
+
+import java.awt.Graphics;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Producer extends Thread {
+	private NumberStore store;
+	private int number;
+
+	public Producer(NumberStore ns, int n) {
+		store = ns;
+		number = n;
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			store.put(i);
+			// System.out.println("Producer #"+ this.number + " put: " + i);
+			try {
+				sleep((int) (Math.random() * 10));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+class Consumer extends Thread {
+	private NumberStore store;
+	private int number;
+
+	public Consumer(NumberStore ns, int n) {
+		store = ns;
+		number = n;
+	}
+
+	public void run() {
+		int value = 0;
+		for (int i = 0; i < 10; i++) {
+			value = store.get();
+			System.out.println("Consumer #" + this.number + " got: " + value);
+			try {
+				sleep((int) (Math.random() * 10));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------------
+
+class NumberStore {
+	private int contents = 0;
+	private boolean available = false;
+
+	public synchronized int get() {
+		while (available == false) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		available = false;
+		notify();
+		return contents;
+	}
+
+	public synchronized void put(int value) {
+		while (available == true) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		contents = value;
+		available = true;
+		notify();
+	}
+}
+
+//---------------------------------------------------------------------------------------
+public class Sliced1 {
+	public static void main(String[] args) {
+		NumberStore ns = new NumberStore();
+		Producer p = new Producer(ns, 1);
+		Consumer c = new Consumer(ns, 1);
+		p.start();
+		c.start();
+	}
+}
