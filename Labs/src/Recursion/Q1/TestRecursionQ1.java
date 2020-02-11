dcba592b08102e8f0d7357d78ecc1fe4fd830467
A00268807 <A00268807@X206-A07.studentnet.ait.ie>
2020-02-11 15:04:41 +0000

    Need To work on this

---
+++ Labs/src/Recursion/Q1/TestRecursionQ1.java
@@ -0,0 +1,7 @@
+package Recursion.Q1;
+
+public class TestRecursionQ1 {
+	public static void main(String[] args) {
+		MyFrameRecursionQ1 mf2 = new MyFrameRecursionQ1("First");
+	}
+}
---
+++ Labs/src/Recursion/Q2/TestRecursionQ2.java
@@ -0,0 +1,7 @@
+package Recursion.Q2;
+
+public class TestRecursionQ2 {
+	public static void main(String[] args) {
+		MyFrameRecursionQ2 mf2 = new MyFrameRecursionQ2("First");
+	}
+}
